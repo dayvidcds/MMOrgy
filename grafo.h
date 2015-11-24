9aa826f09108de515fdeeefba38e9730316f0bde
João Neto <j.alexandreeneto@gmail.com>
2015-11-24 20:10:58 -0300

    arquivos cabeçalhos comentados e arquivos de clusterização criados

--- grafo.h
+++ grafo.h
@@ -10,6 +10,8 @@
 	FEM = 2
 };*/
 
+
+/* estruturas para encadeamento de amigos e interesses */
 struct Amigos{
 	int indice;
 	struct Amigos *prox_amigo;
@@ -20,6 +22,7 @@ struct Interesses{
 	struct Interesses *prox_interesse;
 };
 
+/* definição da estrutura de Perfil dos usuários */
 struct Perfil{
 	char nome[128];
 	char cidade[64];
@@ -30,12 +33,14 @@ struct Perfil{
 	struct Interesses *interesses;
 };
 
+/* definição da estrutura do Nós do Grafo */
 struct No{
 	int visitado;
 	struct Perfil perfil;
 	struct No *no_prox;
 };
 
+/* função que monta o grafo, dado um arquivo de entrada */
 struct No *montar_grafo (FILE *entrada);
 
 #endif
--- search.h
+++ search.h
@@ -1,15 +1,26 @@
 #ifndef __SEARCH_H__
 #define __SEARCH_H__
 
+/* definição da estrutura dos Nós da Fila */
 struct NoQueue{
 	struct No *no;
 	struct NoQueue *fila_prox;
 };
 
+/* verifica quais nós já foram visitados */
 int verif_visitas (struct No **head);
+
+/* reseta todas as visitas */
 void reset_visitas (struct No **head);
+
+/* dado um indice/posição, devolve o nó correspondente
+	na lista de adjacências  */
 struct No *list_get (struct No **head, int indice);
+
+/* enfileira um elemento */
 void enqueue (struct NoQueue **fila_fim, struct NoQueue *fila_novo);
+
+/* função que efetua a busca em largura BFS */
 struct No *buscaLargura (struct No *grafo, int index_Inicio, const char *nomePessoa, int *numCidades);
 
 #endif
